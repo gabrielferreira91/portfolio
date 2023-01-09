e7ce1b152a3cf7a9841659d5c07c1858d2817b50
gabrielferreira91 <66332615+gabrielferreira91@users.noreply.github.com>
2023-01-08 22:37:19 -0300

    Add files via upload
    Site finalizado dia 08/01/2023

---
+++ scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 24) {
+      $('.navbar').addClass('sticky')
+    } else {
+      $('.navbar').removeClass('sticky')
+    }
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show')
+    } else {
+      $('.scroll-up-btn').removeClass('show')
+    }
+  })
+})
+
+$('.scroll-up-btn').click(function () {
+  $('html').animate({ scrollTop: 0 })
+})
+
+var typed = new Typed('.typing', {
+  strings: ['Desenvolvedor', 'Designer', 'Freelancer', 'Arte Finalista'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+})
+var typed = new Typed('.typing-2', {
+  strings: ['Desenvolvedor', 'Designer', 'Freelancer', 'Arte Finalista'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+})
+
+$('.menu-btn').click(function () {
+  $('.navbar .menu').toggleClass('active')
+  $('.menu-btn i').toggleClass('active')
+
+})
+
+function closeMenu() {
+  $('.navbar .menu').toggleClass('active')
+  $('.menu-btn i').toggleClass('active')
+}
